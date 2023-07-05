e1eaf42a1d5548a281c56a608a38430a394eb749
JuliaFSO <juliafso@gmail.com>
2023-07-05 15:28:09 -0400

    Changes the index into public folder

---
+++ build/precache-manifest.2c931e95d8cee314fd9475a086b49c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897b3996ef5f753336603e50f09443d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d32a05f237808e27b9",
+    "url": "/static/css/main.634bb71e.chunk.css"
+  },
+  {
+    "revision": "8a7004204c021a12aa22",
+    "url": "/static/js/2.07203224.chunk.js"
+  },
+  {
+    "revision": "c972e5d9db84f32fc196bae6dd642598",
+    "url": "/static/js/2.07203224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d32a05f237808e27b9",
+    "url": "/static/js/main.b25ac52b.chunk.js"
+  },
+  {
+    "revision": "31fe52e0bb68f83722f1",
+    "url": "/static/js/runtime-main.7094e3bb.js"
+  }
+]);
